2da65020a2aa08fc0275009ae90f658cdeb5e1f6
David Strupinski <david.strupinski@gmail.com>
2022-07-12 10:31:44 -0700

    modularize logic

--- sis_provisioner/dao/student.py
+++ sis_provisioner/dao/student.py
@@ -1,31 +1,49 @@
 from django.conf import settings
+import json
 from uw_person_client import UWPersonClient
 
 
-class UWHandshakeClient(UWPersonClient):
+class UWHandshakeClient:
 
     """
     only campus 0 and 1, enrollment status 12 (16 too?), class codes 1,2,3,4,5, and 8, major codes not 0-EMBA, 0-GEMBA
     don't sync majors for campus 1, college code E (Bothell or Foster)
     """
 
+    def __init__(self):
+        self.client = UWPersonClient()
+
     @staticmethod
     def valid_class_code(class_code):
-        return class_code in getattr(settings, 'INCLUDED_CLASS_CODES', [])
+        class_codes = json.loads(getattr(settings, 'INCLUDED_CLASS_CODES', '[]'))
+        return class_code in class_codes
 
     @staticmethod
     def valid_campus_code(campus_code):
-        return campus_code in getattr(settings, 'INCLUDED_CAMPUS_CODES', [])
+        campus_codes = json.loads(getattr(settings, 'INCLUDED_CAMPUS_CODES', '[]'))
+        return campus_code in campus_codes
 
     @staticmethod
     def valid_major_code(major_codes):
-        for excluded_code in getattr(settings, 'EXCLUDED_MAJOR_CODES', []):
+        excluded_codes = json.loads(getattr(settings, 'EXCLUDED_MAJOR_CODES', '[]'))
+        for excluded_code in excluded_codes:
             if excluded_code in major_codes:
                 return False
         return True
 
+    def _update_college_for_majors(self, majors):
+        for major in majors:
+            if major.major_abbr_code in \
+                    ['0-BIOEN', '0-BSE', '0-DATA','0-PHARBX', '0-PREBSE',
+                        '0-TECH I', '0-C SCI', '0-CMP E', '0-CSE']:
+                major.college = 'J'
+
+    def _get_handshake_major(self, majors):
+        # most alphabetically last major college code
+        return max(major.college for major in majors)
+
     def get_students_for_handshake(self):
-        students = self.get_registered_students(
+        students = self.client.get_registered_students(
             include_employee=False,
             include_student_transcripts=False,
             include_student_transfers=False,
@@ -38,27 +56,20 @@ class UWHandshakeClient(UWPersonClient):
         handshake_students = []
 
         for person in students:
-            if not valid_class_code(person.student.class_code):
+            if not self.valid_class_code(person.student.class_code):
                 continue
 
-            if not valid_campus_code(person.student.campus_code):
+            if not self.valid_campus_code(person.student.campus_code):
                 continue
 
-            if not valid_major_code(person.student.majors):
+            if not self.valid_major_code(person.student.majors):
                 continue
 
-            majors = []
-            for major in person.student.majors:
-                if major in ['0-BIOEN', '0-BSE', '0-DATA', '0-PHARBX', '0-PREBSE', '0-TECH I']:
-                    major.college = 'J'
-                elif major in ['0-C SCI', '0-CMP E', '0-CSE']:
-                    major.college = 'J' # code for 'College of Computer Science & Engineering' is just the College of Engineering?
-                majors.append(major.college)
-            synced_major = max(college for college in majors)
-            if synced_major == 'E':
-                person.student.majors = []
+            self._update_college_for_majors(person.student.majors)
 
-            if person.student.campus_code == '1':
+            synced_major = self._get_handshake_major(person.student.majors)
+            # no major syncing for Foster College or Bothell Campus
+            if synced_major == 'E' or person.student.campus_code == '1':
                 person.student.majors = []
 
             handshake_students.append(person)
