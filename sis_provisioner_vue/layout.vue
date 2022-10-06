be84fa659946096d048e7e5e85e0e21b93ac81ea
Jim Laney <jlaney@uw.edu>
2022-10-06 15:08:38 -0700

    don't exclude undeclared majors

--- sis_provisioner_vue/layout.vue
+++ sis_provisioner_vue/layout.vue
@@ -19,11 +19,13 @@
         that have been imported to Handshake.
         <br /><br />
         To import a new file:
-        <br/>
+        <br/><br/>
         <ol>
           <li>Click &quot;Create new file&quot;, and select the enrollment term.</li>
+          <br/>
           <li>The &quot;CSV Generated&quot; column indicates the status of the
           file-creation process</li>
+          <br/>
           <li>Once created, click &quot;Import to Handshake&quot; to import the file.</li>
         </ol>
       </p>
