d76d91f79f5cf6d5be0df13d76aaeb248212da82
Jim Laney <jlaney@uw.edu>
2022-10-06 08:51:36 -0700

    add instructions to layout

--- sis_provisioner_vue/layout.vue
+++ sis_provisioner_vue/layout.vue
@@ -15,10 +15,17 @@
     </template>
     <template #aside>
       <p class="text-light-gray bg-dark-purple rounded-3 p-3 small">
-        Welcome to the Handshake Import tool!
+        Welcome to the Handshake Import tool! On this page, you can browse files
+        that have been imported to Handshake.
         <br /><br />
-        Browse files that have been imported to Handshake.
-        <br /><br />
+        To import a new file:
+        <br/>
+        <ol>
+          <li>Click &quot;Create new file&quot;, and select the enrollment term.</li>
+          <li>The &quot;CSV Generated&quot; column indicates the status of the
+          file-creation process</li>
+          <li>Once created, click &quot;Import to Handshake&quot; to import the file.</li>
+        <ol>
       </p>
     </template>
     <template #main>
