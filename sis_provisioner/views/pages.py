e79156f21840238714caf14fc9a308d58fedaf5e
Jim Laney <jlaney@uw.edu>
2023-01-03 15:41:09 -0800

    update copyright year

--- sis_provisioner/views/pages.py
+++ sis_provisioner/views/pages.py
@@ -1,5 +1,6 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
