a0136c0a453c2bb44d20fe84dd7c7121500b2e49
Jim Laney <jlaney@uw.edu>
2024-04-11 15:34:47 -0700

    add app_start.sh

---
+++ docker/app_start.sh
@@ -0,0 +1,19 @@
+if [ "$ENV" = "localdev" ]
+then
+
+  echo "Waiting for postgres..."
+
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+
+  source "/app/bin/activate"
+
+  cd /app
+  python manage.py migrate
+  python manage.py initialize_db
+  python manage.py loaddata person employee term major student adviser transfer transcript hold degree sport
+
+fi
